52b49a009aee4b4bf90a6c6652efe0b6ec303d3b
Tanvir-Bhuiyan00 <mdtanvir95925@gmail.com>
2023-05-02 17:27:21 +0600

    food_posts.json added

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const posts = require("./data/food_posts.json");
 
 app.get("/", (req, res) => {
   res.send("Recipe & Riot site is running");
 });
 
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
 app.listen(port, () => {
   console.log(`Recipe & Riot API is running on port: ${port}`);
