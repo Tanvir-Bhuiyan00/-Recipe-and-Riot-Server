a0a6184767ae6313526edec67d0dc84c5c8c4f9a
Tanvir-Bhuiyan00 <mdtanvir95925@gmail.com>
2023-05-02 20:26:30 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const posts = require("./data/food_posts.json");
 
-const chef = require("./data/chef.json")
+const chef = require("./data/chef.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Recipe & Riot site is running");
@@ -15,11 +17,15 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-
 app.get("/chef", (req, res) => {
   res.send(chef);
 });
 
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chef.find((c) => c.id === id);
+  res.send(selectedChef);
+});
 
 app.listen(port, () => {
   console.log(`Recipe & Riot API is running on port: ${port}`);
